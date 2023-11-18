d73458637a6aa0d9992369d4bb29a94dd8a85c88
Shanyi <1215065923@qq.com>
2023-11-18 15:28:06 +0800

    feat: init command

--- pattern_command.py
+++ pattern_command.py
@@ -1,20 +1,22 @@
 FILE_COMMAND = "file"
 EDIT_COMMAND = "edit"
 DISPLAY_COMMAND = "display"
+LOG_COMMAND = "log"
+STAT_COMMAND = "stat"
 
 # Command Interface
 class Command:
-    type = None
     def execute(self):
         pass
 
 # Concrete Command Classes
 
-# load test.md
+# load 文件路径
 class LoadCommand(Command):
-    Command.type = FILE_COMMAND
-    def __init__(self, file_path):
+    def __init__(self, processor, file_path):
+        self.type = FILE_COMMAND
         self.file_path = file_path
+        self.processor = processor
     
     def execute(self):
         # Implementation for load command
@@ -22,21 +24,151 @@ class LoadCommand(Command):
 
 # save
 class SaveCommand(Command):
-    Command.type = FILE_COMMAND
-    def __init__(self, file_name):
+    def __init__(self, processor, file_name):
+        self.type = FILE_COMMAND
         self.file_name = file_name
+        self.processor = processor
     
     def execute(self):
         # Implementation for load command
         pass
+
+# ws
+class WSCommand(Command):
+    Command.type = FILE_COMMAND
+    def __init__(self, processor):
+        self.processor = processor
     
+    def execute(self):
+        # Implementation for load command
+        pass
+
+# switch 文件序号
+class SwitchCommand(Command):
+    def __init__(self, processor, file_index):
+        self.type = FILE_COMMAND
+        self.file_index = file_index
+        self.processor = processor
     
+    def execute(self):
+        # Implementation for load command
+        pass
+
+# close 文件序号
+class CloseCommand(Command):
+    def __init__(self, processor, file_index):
+        self.type = FILE_COMMAND
+        self.file_index = file_index
+        self.processor = processor
+    
+    def execute(self):
+        # Implementation for load command
+        pass
+
+# insert [⾏号] 标题/文本
 class InsertCommand(Command):
+    def __init__(self, processor, text, line_number = -1):
+        self.type = EDIT_COMMAND
+        self.processor = processor
+        self.text = text
+        self.line_number = line_number
+        
     def execute(self):
         # Implementation for insert command
         pass
 
+# append-tail 标题/文本
+class AppendTailCommand(Command):
+    def __init__(self, processor, text):
+        self.type = EDIT_COMMAND
+        self.processor = processor
+        self.text = text
+        
+    def execute(self):
+        # Implementation for append-tail command
+        pass
+
+# delete 标题/文本 或delete ⾏号
+class DeleteCommand(Command):
+    def __init__(self, processor, text = "", line_number = -1):
+        self.type = EDIT_COMMAND
+        self.processor = processor
+        self.text = text
+        self.line_number = line_number
+        
+    def execute(self):
+        # Implementation for delete command
+        pass
+    
+# undo
 class UndoCommand(Command):
+    def __init__(self, processor):
+        self.type = EDIT_COMMAND
+        self.processor = processor
+    
     def execute(self):
         # Implementation for undo command
+        pass
+
+# redo
+class RedoCommand(Command):
+    def __init__(self, processor):
+        self.type = EDIT_COMMAND
+        self.processor = processor
+    
+    def execute(self):
+        # Implementation for redo command
+        pass
+
+# list
+class ListCommand(Command):
+    def __init__(self, processor):
+        self.type = DISPLAY_COMMAND
+        self.processor = processor
+    
+    def execute(self):
+        # Implementation for list command
+        pass
+
+# list-tree
+class ListTreeCommand(Command):
+    def __init__(self, processor):
+        self.type = DISPLAY_COMMAND
+        self.processor = processor
+    
+    def execute(self):
+        # Implementation for list-tree command
+        pass
+
+# dir-tree [⽬录]
+class DirTreeCommand(Command):
+    def __init__(self, processor, dir_path):
+        self.type = DISPLAY_COMMAND
+        self.processor = processor
+        self.dir_path = dir_path
+    
+    def execute(self):
+        # Implementation for dir-tree command
+        pass
+
+# history [数量]c
+class HistoryCommand(Command):
+    def __init__(self, processor, count = -1):
+        self.type = LOG_COMMAND
+        self.processor = processor
+        self.count = count
+    
+    def execute(self):
+        # Implementation for history command
+        pass
+
+# stats [all | current]
+class StatsCommand(Command):
+    def __init__(self, processor, stats_type = 'all'):
+        self.type = STAT_COMMAND
+        self.processor = processor
+        self.stats_type = stats_type
+    
+    def execute(self):
+        # Implementation for stats command
         pass
